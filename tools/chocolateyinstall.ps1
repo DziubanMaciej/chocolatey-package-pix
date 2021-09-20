e0cea1863a84df6b6cc4576f3ec7c238f9475d0d
MaciejDziuban <dziuban.maciej@gmail.com>
2021-09-20 19:42:23 +0200

    Update to 2108.18

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWFWkd'
+$url = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWJymh'
 
 $packageArgs = @{
   packageName   = 'pix'
@@ -11,7 +11,7 @@ $packageArgs = @{
   silentArgs    = '/install /quiet'
   validExitCodes= @(0)
   softwareName  = '*pix*'
-  checksum      = '7205D3ECD5692BC53A5515F35E7E2D564E6E62901FC37BC5D067C0A2B902C3D3'
+  checksum      = 'CA3098327B061077F16311AD016F22761E69A66964A227192D924A7C2BC4B366'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
