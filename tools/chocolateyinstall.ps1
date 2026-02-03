975f24559c0c30c1c0c469d6d10731105533639e
MaciejDziuban <dziuban.maciej@gmail.com>
2026-02-04 00:30:59 +0100

    Update to 2601.15

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RW1e7km'
+$url = 'https://download.microsoft.com/download/9dd61174-c7bb-4cb2-8e69-17f4df02a190/PIX-2601.15-Installer-x64.exe'
 
 $packageArgs = @{
   packageName   = 'pix'
@@ -11,7 +11,7 @@ $packageArgs = @{
   silentArgs    = '/install /quiet'
   validExitCodes= @(0)
   softwareName  = '*pix*'
-  checksum      = 'CD44C578E898340F033C3A88D5D67531BF1FBB3CD647FA5F5BE1D5496388B55B'
+  checksum      = 'DFA07BBDF3B6E8EE651A1032A885850E5632CE313695770C13950B12BFE68CB7'
   checksumType  = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
